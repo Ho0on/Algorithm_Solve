0b7444721310fc1b2f20e5670ee42506de235913
SangHoon <88081661+Ho0on@users.noreply.github.com>
2021-11-22 20:38:41 +0900

    Solve: 21.11.22

---
+++ CK_로마자에서 숫자로.js
@@ -0,0 +1,47 @@
+// 로마자에서 숫자로 바꾸기
+
+// 1~3999 사이의 로마자 s를 인자로 주면 그에 해당하는 숫자를 반환해주세요. 로마 숫자를 숫자로 표기하면 다음과 같습니다.
+
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+// 로마자를 숫자로 읽는 방법은 로마자를 왼쪽부터 차례대로 더하면 됩니다. III = 3 XII = 12 XXVII = 27 입니다.
+
+// 그런데 4를 표현할 때는 IIII가 아니라 IV 입니다. 뒤의 숫자에서 앞의 숫자를 빼주면 됩니다. 9는 IX입니다.
+
+// I는 V와 X앞에 와서 4, 9 X는 L, C앞에 와서 40, 90 C는 D, M앞에 와서 400, 900
+
+function romanToNum(s) {
+  const symbolValue={
+    "I":1,
+    "V":5,
+    "X":10,
+    "L":50,
+    "C":100,
+    "D":500,
+    "M":1000
+}
+  const result=[];
+  let arrSymbol=s.split('');
+
+  for(let i=0;i<arrSymbol.length;i++){
+    result.push(symbolValue[arrSymbol[i]])
+  }
+
+  let finalResult=0;
+
+  for(let j=0;j<result.length;j++){
+    if(result[j]<result[j+1]){
+      finalResult += result[j+1]-result[j];
+      j+=1;
+    }else{
+      finalResult += result[j];
+    }
+  }
+  return finalResult;
+}
